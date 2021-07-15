6d2b7c3a4244c3fdcd732ef0f3e0cecd946e7e35
Samuel Santos <samuxlh@gmail.com>
2021-07-15 14:59:19 -0300

    updates 15/7

--- src/App.js
+++ src/App.js
@@ -16,36 +16,49 @@ import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js';
 function App() {
 
   function setTabActive(active) {
-    const linkList = document.getElementsByClassName('ID001');
-    switch(active){
+    let linkList = document.getElementsByClassName('ID001');
+    switch (active) {
       case 0:
         linkList[0].classList.add('menuLink-active');
         linkList[1].classList.remove('menuLink-active');
         linkList[2].classList.remove('menuLink-active');
         linkList[3].classList.remove('menuLink-active');
-        return null;
+        break;
       case 1:
         linkList[1].classList.add('menuLink-active');
         linkList[0].classList.remove('menuLink-active');
         linkList[2].classList.remove('menuLink-active');
         linkList[3].classList.remove('menuLink-active');
-        return null;
+        break;
       case 2:
         linkList[2].classList.add('menuLink-active');
         linkList[1].classList.remove('menuLink-active');
         linkList[0].classList.remove('menuLink-active');
         linkList[3].classList.remove('menuLink-active');
-        return null;
+        break;
       case 3:
         linkList[3].classList.add('menuLink-active');
         linkList[1].classList.remove('menuLink-active');
         linkList[2].classList.remove('menuLink-active');
         linkList[0].classList.remove('menuLink-active');
-        return null;
-      
-        default:
-        return null;
+        break;
+
+      default:
+        break;
     }
+  }
+
+  function syncHeaderURL(){
+  const pageURL = window.location.href;
+  if (pageURL.includes('about')) {
+    setTabActive(1);
+  } else if (pageURL.includes('projects')) {
+    setTabActive(2);
+  } else if (pageURL.includes('contact')) {
+    setTabActive(3);
+  } else {
+    setTabActive(0);
+  }
   }
 
   return (
@@ -53,10 +66,11 @@ function App() {
       <Router>
         <div className="alternativeFont">
           <ul className="headerLinkList">
-            <li className="menuLink" onClick={() => setTabActive(0)}><Link className="ID001 menuLink-active" to="/">Home</Link></li>
+            <li className="menuLink" onClick={() => setTabActive(0)}><Link className="ID001" to="/">Home</Link></li>
             <li className="menuLink" onClick={() => setTabActive(1)}><Link className="ID001" to="/about">Who am I?</Link></li>
             <li className="menuLink" onClick={() => setTabActive(2)}><Link className="ID001" to="/projects">My Projects</Link></li>
             <li className="menuLink" onClick={() => setTabActive(3)}><Link className="ID001" to="/contact">Wanna contact me?</Link></li>
+            
           </ul>
         </div>
         <main className="defaultContainer">
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,14 +5,13 @@ function Contact() {
         <div className="textFont posRelative">
             <p className="textAlignCenter">Don't be shy. You can message me at any of these:</p>
             <div className="socialBox">
-                    <i className="fab fa-github fa-5x"></i>
-                    <i className="fab fa-twitter fa-5x"></i>
-                    <i className="fab fa-twitch fa-5x"></i>
-                    <i className="far fa-envelope fa-5x"></i>
-                    <i className="fab fa-linkedin fa-5x"></i>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/samuxlh"><i className="fab fa-github fa-5x"></i></a>
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/nonicksam"><i className="fab fa-twitter fa-5x"></i></a>
+                    <a target="_blank" rel="noreferrer" href="https://www.twitch.tv/vvssam"><i className="fab fa-twitch fa-5x"></i></a>
+                    <a target="_blank" rel="noreferrer" href="mailto:samuxlh@gmail.com"><i className="far fa-envelope fa-5x"></i></a>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/samuelcsantos/"><i className="fab fa-linkedin fa-5x"></i></a>
             </div>
         </div>
-
     )
 }
 
