ae8679e4bcdcdb32a241edf4e3759e07bfc9cd55
Samuel Santos <samuel.cezar@ykt.com.br>
2021-11-08 08:22:12 -0300

    typo at homepage... yikes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
             <p className="textFont defaultText">Welcome to my page, my name is</p>
             <h1 className="titleFont bigText"><Typist cursor={{ hideWhenDone: true }}>Samuel Santos</Typist></h1>
             <p className="textFont defaultText">But you can call me Sam if you want to.</p>
-            <p className="textFont defaultText">I like games, cybersec, data science, Python, React. You can find my navigate using the bar on top...</p>
+            <p className="textFont defaultText">I like React, Python, games, cybersec and data science. You can find more info using the bar on top...</p>
             </span>
         </div>
     )
