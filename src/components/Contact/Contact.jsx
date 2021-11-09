aaca9f4a68a3242018093717deb6f5bda84afd68
Samuel Santos <samuel.cezar@ykt.com.br>
2021-11-09 10:26:25 -0300

    + lighter images + contact subtext links working again

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,19 +7,19 @@ function Contact() {
             <div className="socialBox">
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1em'}}>
                     <a target="_blank" rel="noreferrer" href="https://github.com/samuxlh"><i className="fab fa-github fa-3x"></i></a>
-                    <span>/samuxlh</span>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/samuxlh"><span>/samuxlh</span></a>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1em' }}>
                     <a target="_blank" rel="noreferrer" href="https://www.twitch.tv/vvssam"><i className="fab fa-twitch fa-3x"></i></a>
-                    <span>/vvssam</span>
+                    <a target="_blank" rel="noreferrer" href="https://www.twitch.tv/vvssam"><span>/vvssam</span></a>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1em' }}>
                     <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/samuelcsantos/"><i className="fab fa-linkedin fa-3x"></i></a>
-                    <span>/samuelcdossantos</span>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/samuelcsantos/"><span>/samuelcdossantos</span></a>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1em' }}>
                     <a target="_blank" rel="noreferrer" href="mailto:samuelcdossantos@hotmail.com"><i className="far fa-envelope fa-3x"></i></a>
-                    <span>Contact through email</span>
+                    <a target="_blank" rel="noreferrer" href="mailto:samuelcdossantos@hotmail.com"><span>Contact through email</span></a>
                 </div>
             </div>
         </div>
