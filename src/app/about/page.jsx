8d235c2d5f4085711d512ef713fae95fd7ef885c
Samuel Santos <samuxlh@gmail.com>
2024-02-27 06:07:05 -0300

    ux hotfix

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -293,24 +293,24 @@ const AboutPage = () => {
                 <div className="w-1/3 "></div>
               </div>
             </motion.div>
-          <Link href="/portfolio">
-            <div className="text-2xl pt-16">
-              {text.split("").map((letter, index) => (
-                <motion.span
-                  key={index}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{
-                    duration: 1,
-                    repeat: Infinity,
-                  }}
-                >
-                  {letter}
-                </motion.span>
-              ))}
-            </div>
-          </Link>
           </div>
+            <div className="text-2xl mb-5 text-center">
+              <Link href="/portfolio">
+                {text.split("").map((letter, index) => (
+                  <motion.span
+                    key={index}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{
+                      duration: 1,
+                      repeat: Infinity,
+                    }}
+                  >
+                    {letter}
+                  </motion.span>
+                ))}
+              </Link>
+            </div>
         </div>
         {/* SVG CONTAINER */}
         <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
