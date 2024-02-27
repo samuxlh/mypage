1694159259a27afe263820dbd76f7c1d753efeda
Samuel Santos <samuxlh@gmail.com>
2024-02-27 05:42:09 -0300

    call to action on landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import NavLink from "../components/navLink";
 import { motion } from "framer-motion";
 
 const Homepage = () => {
+  const text = "Click here to begin...";
+
   return (
     <motion.div
       className="h-full"
@@ -13,10 +16,6 @@ const Homepage = () => {
       transition={{ duration: 0.5 }}
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        {/* IMAGE CONTAINER 
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <Image src="/hero.png" alt="" fill className="object-contain" />
-        </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
@@ -25,9 +24,26 @@ const Homepage = () => {
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-          Hello! Welcome to my page.
-          You can explore more about my journey and areas of expertise using the menu.
+            Hello! Welcome to my page.
+            You can explore more about my journey and areas of expertise using the menu.
           </p>
+          <Link href="/about">
+          <div className="text-3xl">
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 1,
+                  repeat: Infinity,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+          </div>
+          </Link>
           {/* BUTTONS */}
         </div>
       </div>
