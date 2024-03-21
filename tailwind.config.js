62c84aa2d756610d27d22790b148524a7b20c858
Samuel Santos <samuxlh@gmail.com>
2024-03-21 12:52:09 -0300

    tailwind custom screen size added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'xs': {'max': '400px'},
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
